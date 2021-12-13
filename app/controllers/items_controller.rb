b3477be00d95b233d5d20f664f3a35a873f37a6c
岡駿哉 <okaj.g123@gamil.com>
2021-12-13 15:08:09 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :contributor_confirmation, only: [:edit]
 
   def index
     @items = Item.order("created_at DESC")
@@ -22,10 +23,30 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    redirect_to root_path unless current_user.id == @item.user_id
+  end
+ end
+
+  def update
+    @item = Item.find(params[:id])
+   if @item.update(item_params)
+    redirect_to item_path
+   else
+    render :edit
+   end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :burden_id, :prefecture_id, :ship_day_id, :cost).merge(user_id: current_user.id)
   end
 
-end
+  def contributor_confirmation
+    redirect_to new_user_session_path unless user_signed_in?
+  end
+
+
+
